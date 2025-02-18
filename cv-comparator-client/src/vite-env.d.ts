e055e3caf7cf6e90b3184d63687e8b36029c641f
unknown <labhalmehdi@gmail.com>
2025-02-18 23:18:04 +0100

    build fix

--- cv-comparator-client/src/vite-env.d.ts
+++ cv-comparator-client/src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_API_URL: string
+    // autres variables d'environnement...
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv
+  }
