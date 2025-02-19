11bee9345255e1a4effde8715ce0d4066a652778
unknown <labhalmehdi@gmail.com>
2025-02-19 17:29:25 +0100

    app.js CORS update frontend URL

--- cv-comparator-server/src/app.js
+++ cv-comparator-server/src/app.js
@@ -15,10 +15,7 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-  origin: [
-  'http://localhost:5173',
-  process.env.FRONTEND_URL|| 'http://localhost:5173'
-  ],
+  origin: process.env.FRONTEND_URL,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
